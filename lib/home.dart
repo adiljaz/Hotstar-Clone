4dc6603e19e536231f8be150519fa49ef3e07646
adiljaz <146193187+adiljaz@users.noreply.github.com>
2024-03-15 20:43:04 +0530

    lated relseases

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hostar_clone_1/colors/colors.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:tmdb_api/tmdb_api.dart';
 
@@ -13,6 +14,10 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int activeIndex = 0;
   List trendingmovies = [];
+  List topratedmovies = [];
+  List tv = [];
+  List upcomingmovies = [];
+
   final String apikey = '9280f0d6b5fe07ce79819ad96f9a2fa0';
   final readaccesstoken =
       'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjgwZjBkNmI1ZmUwN2NlNzk4MTlhZDk2ZjlhMmZhMCIsInN1YiI6IjY1ZjJkODk5NDcwZWFkMDE3ZTlmOWU4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JRGCDCp81lbprD-uaYM1Po5Cn9dWCQckvcfhGsh4X-k';
@@ -31,8 +36,14 @@ class _HomeState extends State<Home> {
         ));
 
     Map trendingResults = await tmdbwithCustomLogs.v3.trending.getTrending();
+    Map topratedresult = await tmdbwithCustomLogs.v3.movies.getTopRated();
+    Map tvresult = await tmdbwithCustomLogs.v3.tv.getPopular();
+    Map upcomingresult = await tmdbwithCustomLogs.v3.movies.getUpcoming();
     setState(() {
       trendingmovies = trendingResults['results'];
+      topratedmovies = topratedresult['results'];
+      tv = tvresult['results'];
+      upcomingmovies = upcomingresult['results'];
     });
   }
 
@@ -54,24 +65,23 @@ class _HomeState extends State<Home> {
                       itemCount: trendingmovies.length,
                       itemBuilder: (context, index, pageviewindex) {
                         return Container(
+                          margin: EdgeInsets.symmetric(horizontal: 7),
                           height: 100,
                           width: double.infinity,
                           decoration: BoxDecoration(
-                            color: Colors.red,
                             image: DecorationImage(
-                              image: NetworkImage(
-                                
-                                
+                                image: NetworkImage(
                                   'https://image.tmdb.org/t/p/w500' +
-                                      trendingmovies[index]['poster_path'],),
-                                      fit:BoxFit.fill 
-                            ),
+                                      trendingmovies[index]['poster_path'],
+                                ),
+                                fit: BoxFit.fill),
                           ),
                         );
                       },
                       options: CarouselOptions(
+                        aspectRatio: 15 / 17,
                         autoPlay: true,
-                        viewportFraction: 1,
+                        viewportFraction: 0.8,
                         autoPlayInterval: Duration(seconds: 2),
                         onPageChanged: (index, reason) {
                           setState(() {
@@ -87,7 +97,7 @@ class _HomeState extends State<Home> {
                         width: 150,
                       ),
                       right: 250,
-                      bottom: 100,
+                      bottom: 305,
                     ),
                     Positioned(
                       child: Icon(Icons.cast),
@@ -103,7 +113,7 @@ class _HomeState extends State<Home> {
                   Container(
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
+                      color: ColorManager.gray,
                     ),
                     height: mediaquery.height * 0.06,
                     width: mediaquery.width * 0.5,
@@ -117,8 +127,8 @@ class _HomeState extends State<Home> {
                         Text(
                           'Watch Now',
                           style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
                           ),
                         ),
                       ],
@@ -130,7 +140,7 @@ class _HomeState extends State<Home> {
                     width: mediaquery.width * 0.15,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Colors.grey,
+                      color: ColorManager.gray,
                     ),
                     child: Icon(Icons.add),
                   ),
@@ -144,7 +154,7 @@ class _HomeState extends State<Home> {
                   'Latest Releases ',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 20,
+                    fontSize: 18,
                   ),
                 ),
               ),
@@ -155,17 +165,28 @@ class _HomeState extends State<Home> {
                   child: ListView.builder(
                     scrollDirection: Axis.horizontal,
                     padding: EdgeInsets.all(2),
-                    itemCount: 30,
+                    itemCount: topratedmovies.length,
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(2.0),
-                        child: Container(
-                          height: 55,
-                          width: 110,
-                          color: Colors.green,
-                        ),
-                      );
+                      if (topratedmovies.isNotEmpty) {
+                        return Padding(
+                          padding: const EdgeInsets.all(2.0),
+                          child: Container(
+                            height: 55,
+                            width: 110,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(3),
+                                image: DecorationImage(
+                                    image: NetworkImage(
+                                      'https://image.tmdb.org/t/p/w500' +
+                                          topratedmovies[index]['poster_path'],
+                                    ),
+                                    fit: BoxFit.fill)),
+                          ),
+                        );
+                      } else {
+                        return null;
+                      }
                     },
                   ),
                 ),
