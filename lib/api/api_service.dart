be2cb5fff2b4db58c926474c2675e0c0fd373d08
adiljaz <146193187+adiljaz@users.noreply.github.com>
2024-03-20 13:15:05 +0530

    .

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 final String apikey = '9280f0d6b5fe07ce79819ad96f9a2fa0';
 final readaccesstoken =
     'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjgwZjBkNmI1ZmUwN2NlNzk4MTlhZDk2ZjlhMmZhMCIsInN1YiI6IjY1ZjJkODk5NDcwZWFkMDE3ZTlmOWU4ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JRGCDCp81lbprD-uaYM1Po5Cn9dWCQckvcfhGsh4X-k';
-String imageUrl = 'https://image.tmdb.org/t/p/w500';
+String imageUrl = 'https://image.tmdb.org/t/p/w500';  
 
 Future<void> trendingMovies() async {
   try {
